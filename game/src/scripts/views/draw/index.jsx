89d3902672aa79b3031dc7020391a6449a624564
lanrongqi <lancelrq@gmail.com>
2021-05-25 17:59:57 +0800

    add pencil

--- game/src/scripts/views/draw/index.jsx
+++ game/src/scripts/views/draw/index.jsx
@@ -1,8 +1,8 @@
 import '@/styles/draw_sth.scss';
-import React, { useEffect, useRef } from 'react';
-
+import React, { useEffect, useRef, useState } from 'react';
+import { GithubPicker } from 'react-color';
 import {
-  AppBar, Toolbar,
+  AppBar, Toolbar, Grid, Slider,
   Typography, Button, IconButton
 } from '@material-ui/core';
 import {
@@ -11,20 +11,44 @@ import {
   Brush as BrushIcon,
   Delete as DeleteIcon,
   ClearAllRounded as ClearAllRoundedIcon,
-  Undo as UndoIcon
+  Undo as UndoIcon,
+  ColorLens as ColorLensIcon
 } from '@material-ui/icons';
 import { DrawBoard } from './utils';
+
+const COLORS_LIST = [
+  '#000000', '#FF0000', '#faad14', '#ffff00', '#00FF00', '#006B76', '#1273DE', '#004DCF', '#5300EB',
+  '#bfbfbf', '#EB9694', '#fff1b8', '#ffffb8', '#C1E1C5', '#BEDADC', '#C4DEF6', '#BED3F3', '#D4C4FB'
+];
+
+let drawBoard = null;
 
 export const DrawSomethingIndex = () => {
   const canvasRef = useRef();
   const pencilRef = useRef();
-  let drawBoard = null;
+  const [pencilWidth, setPencilWidth] = useState(5);
+  const [pencilColor, setPencilColor] = useState('#000');
+  const [colorPickerVisible, setColorPickerVisible] = useState(false);
+
   useEffect(() => {
     if (canvasRef.current) {
       drawBoard = new DrawBoard(canvasRef.current, pencilRef.current);
       drawBoard.init();
+      drawBoard.setPencilStyle(pencilWidth, pencilColor);
     }
+    return () => {
+      if (drawBoard) {
+        drawBoard.destory();
+        drawBoard = null;
+      }
+    };
   }, []);
+  useEffect(() => {
+    if (drawBoard) {
+      drawBoard.setPencilStyle(pencilWidth, pencilColor);
+    }
+    if (colorPickerVisible) setColorPickerVisible(false);
+  }, [pencilWidth, pencilColor]);
 
   return <div id="app-draw-somethind">
     <AppBar position="static">
@@ -40,31 +64,66 @@ export const DrawSomethingIndex = () => {
     </AppBar>
     <span id="app-draw-pencil" ref={pencilRef} style={{ position: 'absolute', display: 'none', pointerEvents: 'none' }}><CreateIcon /></span>
     <div className="draw-wrapper">
-      <canvas ref={canvasRef} style={{ width: 960, height: 540 }} />
-      <div>
-        <IconButton
-          variant="contained"
-          color="primary"
-        >
-          <BrushIcon />
-        </IconButton>
-        <IconButton
-          variant="contained"
-        >
-          <ClearAllRoundedIcon />
-        </IconButton>
-        <IconButton
-          variant="contained"
-        >
-          <UndoIcon />
-        </IconButton>
-        <IconButton
-          variant="contained"
-          color="secondary"
-        >
-          <DeleteIcon />
-        </IconButton>
-      </div>
+      <canvas ref={canvasRef} width={960} height={540} style={{ width: 960, height: 540 }} />
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Grid container spacing={1}>
+            <Grid item>
+              <IconButton
+                variant="contained"
+                color="primary"
+                onClick={() => { setColorPickerVisible(!colorPickerVisible); }}
+              >
+                <ColorLensIcon style={{ color: pencilColor }} />
+              </IconButton>
+              {colorPickerVisible && <div style={{ position: 'fixed' }}>
+                <GithubPicker
+                  onChange={(color) => { setPencilColor(color.hex); }}
+                  colors={COLORS_LIST}
+                  color={pencilColor}
+                  width={240}
+                />
+              </div>}
+            </Grid>
+            <Grid item xs={3} style={{ lineHeight: '64px' }}>
+              <Slider
+                value={pencilWidth}
+                onChange={(e, v) => { setPencilWidth(v * 1); }}
+                aria-labelledby="discrete-slider"
+                valueLabelDisplay="auto"
+                step={2}
+                marks
+                min={1}
+                max={11}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={6} style={{ textAlign: 'right' }}>
+          <IconButton
+            variant="contained"
+            color="primary"
+          >
+            <BrushIcon />
+          </IconButton>
+          <IconButton
+            variant="contained"
+          >
+            <ClearAllRoundedIcon />
+          </IconButton>
+          <IconButton
+            variant="contained"
+          >
+            <UndoIcon />
+          </IconButton>
+          <IconButton
+            variant="contained"
+            color="secondary"
+          >
+            <DeleteIcon />
+          </IconButton>
+        </Grid>
+      </Grid>
     </div>
   </div>;
 };
--- game/src/scripts/views/draw/utils.ts
+++ game/src/scripts/views/draw/utils.ts
@@ -1,23 +1,66 @@
 export class DrawBoard {
-  canvas:HTMLCanvasElement = null;
+  // canvas dom
+  canvas:HTMLCanvasElement = null
 
+  // canvas context
+  ctx: CanvasRenderingContext2D = null;
+
+  // cursor element
   cursorEl:HTMLElement = null;
+
+  // is drawing
+  drawing:boolean = false;
+
+  pencilWidth: number = 1;
+
+  pencilColor: string = '#000';
 
   constructor(c:HTMLCanvasElement, cursor:HTMLElement) {
     this.canvas = c;
+    this.ctx = this.canvas.getContext('2d');
     this.cursorEl = cursor;
+
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+    this.onMouseUp = this.onMouseUp.bind(this);
   }
 
   init() {
-    this.bindListener();
+    this.canvas.addEventListener('mousedown', this.onMouseDown);
+    this.canvas.addEventListener('mousemove', this.onMouseMove);
+    this.canvas.addEventListener('mouseleave', this.onMouseLeave);
+    this.canvas.addEventListener('mouseup', this.onMouseUp);
   }
 
-  bindListener() {
-    this.canvas.addEventListener('mousemove', this.onMouseMove.bind(this));
-    this.canvas.addEventListener('mouseleave', this.onMouseLeave.bind(this));
+  destory() {
+    this.canvas.removeEventListener('mousedown', this.onMouseDown);
+    this.canvas.removeEventListener('mousemove', this.onMouseMove);
+    this.canvas.removeEventListener('mouseleave', this.onMouseLeave);
+    this.canvas.removeEventListener('mouseup', this.onMouseUp);
+  }
+
+  setPencilStyle(width:number, color:string) {
+    this.pencilWidth = width;
+    this.pencilColor = color;
+  }
+
+  onMouseDown(ev:MouseEvent) {
+    this.drawing = true;
+    this.ctx.beginPath();
+    console.log(this.pencilWidth, this.pencilColor)
+    this.ctx.strokeStyle = this.pencilColor;
+    this.ctx.lineWidth = this.pencilWidth;
+    this.ctx.lineCap = 'round';
+    this.ctx.lineJoin = 'round';
+    this.ctx.moveTo(ev.offsetX, ev.offsetY);
   }
 
   onMouseMove(ev:MouseEvent) {
+    if (this.drawing) {
+      this.ctx.lineTo(ev.offsetX, ev.offsetY);
+      this.ctx.stroke();
+    }
     this.cursorEl.style.display = 'block';
     this.cursorEl.style.left = `${this.canvas.offsetLeft + ev.offsetX - 2}px`;
     this.cursorEl.style.top = `${this.canvas.offsetTop + ev.offsetY - 20}px`;
@@ -27,5 +70,12 @@ export class DrawBoard {
   onMouseLeave() {
     this.cursorEl.style.display = 'none';
     document.body.style.cursor = 'default';
+    this.ctx.closePath();
+    this.drawing = false;
+  }
+
+  onMouseUp() {
+    this.ctx.closePath();
+    this.drawing = false;
   }
 }
